67141208b05c8072f1f3628c9f66446fb5f7d976
Imran Moin <97769980+ImransCodeHub@users.noreply.github.com>
2024-02-19 23:07:29 -0330

    Added new elements to Movie.js Added new json file for movie reviews with data

--- src/Movie.js
+++ src/Movie.js
@@ -6,19 +6,44 @@ rating though its ‘props’ argument.
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
+
+
 const Movie = ({ movie }) => {
     return (
       <Container fluid>
         <Row className="movie-row">
           <Col>
             <h2>{movie.name}</h2>
-            <p><strong>Year:</strong> {movie.year}</p>
             <p><strong>Rating:</strong> {movie.rating}</p>
+            <p><strong>Release Date:</strong> {movie.releaseDate}</p>
+            <p><strong>Description:</strong> {movie.description}</p>
+            <p><strong>Actors:</strong> {movie.actors}</p>
+            <p><strong>Poster:</strong> </p>
+            <img src={movie.poster} alt={movie.name} style={{ width: '100px', height: 'auto' }} />
+
           </Col>
         </Row>
       </Container>
     );
   };
+
+  export default Movie;
+
+
+// Assignment:3
+// const Movie = ({ movie }) => {
+//   return (
+//     <Container fluid>
+//       <Row className="movie-row">
+//         <Col>
+//           <h2>{movie.name}</h2>
+//           <p><strong>Year:</strong> {movie.year}</p>
+//           <p><strong>Rating:</strong> {movie.rating}</p>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
 
 // const Movie = (props) => {
 //   const { movie, key } = props;
@@ -31,5 +56,5 @@ const Movie = ({ movie }) => {
 //   );
 // };
 
-export default Movie;
+
 
